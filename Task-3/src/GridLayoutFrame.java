c5fce1bf97699938f4f911aab65260066cf9a579
aguanegish <aguanegish@gmail.com>
2026-03-20 16:25:05 +0300

    Update GridLayoutFrame.java
    Added two JPanel component

--- Task-3/src/GridLayoutFrame.java
+++ Task-3/src/GridLayoutFrame.java
@@ -3,14 +3,13 @@ import java.awt.*;
 
 public class GridLayoutFrame {
     JFrame frame;
-    JPanel JPanel1;
+    JPanel JPanel1,JPanel2,JPanel3;
 
     public GridLayoutFrame() {
         mainFrame();
     }
 
     public JFrame mainFrame() {
-        //  frame creation
         frame = new JFrame();
 
         frame.setLayout(new GridLayout(1,3));
@@ -18,16 +17,32 @@ public class GridLayoutFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanelComponent1();
+        JPanelComponent2();
+        JPanelComponent3();
 
         frame.setVisible(true);
 
         return frame;
     }
+
     public JPanel JPanelComponent1() {
-        //  JPanel creation
         JPanel1 = new JPanel();
         JPanel1.setBackground(Color.GRAY);
         frame.add(JPanel1);
         return JPanel1;
+    }
+
+    public JPanel JPanelComponent2() {
+        JPanel2 = new JPanel();
+        JPanel2.setBackground(Color.RED);
+        frame.add(JPanel2);
+        return JPanel2;
+    }
+
+    public JPanel JPanelComponent3() {
+        JPanel3 = new JPanel();
+        JPanel3.setBackground(Color.ORANGE);
+        frame.add(JPanel3);
+        return JPanel3;
     }
 }
